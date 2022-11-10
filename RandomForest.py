1bbf742a9ccecf85b20278cec489f350c0a7549d
slaninajenny <117438696+slaninajenny@users.noreply.github.com>
2022-11-10 16:33:54 +0100

    Update RandomForest.py

--- RandomForest.py
+++ RandomForest.py
@@ -3,6 +3,8 @@ from pandas import read_csv
 from sklearn.model_selection import cross_val_score, StratifiedKFold
 from sklearn.ensemble import RandomForestClassifier
 
+#Random Forest Verfahren mit zur Verfügung gestellten csv-Daten
+
 df = read_csv("C://Users//Stefan Schönig//Desktop//Data Analysis II//Übung//FeatureSelection//train.csv")
 X = df.iloc[:,0:20]  #independent columns
 y = df.iloc[:,-1]    #target column i.e price range
